314c6994d1710f406fc6b643b7ebbce485af3630
AbdullahShoaib <abdullahshoaib0333@gmail.com>
2021-08-30 23:41:14 +0500

    done

--- script.js
+++ script.js
@@ -11,26 +11,25 @@ let plus;
 let minus;
 let multiply;
 let division;
-console.log('yo');
 clean.addEventListener('click',()=>{
-    console.log('cc');
     data.innerText='';
     val1=null;
     val2=null;
     operator=null;
 })
 solve.addEventListener('click',()=>{
-    console.log('ss');
     operate(val1,val2,operator);
 })
 sign.forEach((operations)=>{
     operations.addEventListener('click',(e)=>{
+        if (val1==null)
+        {
+            data.innerText="Enter a No.";
+        }else
+        {
         data.innerText='';
-        operator=e.target.value;
-        console.log(operator);
-        data.innerText=operator;
-        console.log(val1);
-        return operator;
+        data.innerText=e.target.value;
+        return operator;}
     })
 })
 bton.forEach((btn) => {
@@ -38,69 +37,51 @@ bton.forEach((btn) => {
             console.log(e.target);
            
         if(operator==null){
-            val1=data.innerText;
-            val1=val1+e.target.value;
-            console.log(val1);
-            console.log('one');  
+            val1=e.target.value+data.innerText;  
             data.innerText=val1;
             return val1;
         }
         else if(operator!==null){ 
                 if(data.innerText==operator){
                     data.innerText='';
-                }  
-            val2=data.innerText;    
-            val2=val2+e.target.value;
-            console.log(val1);
-            console.log(operator);
-            console.log(val2);
-            console.log('two');  
+                }    
+            val2=e.target.value+data.innerText;
             data.innerText=val2;
-        return val2;
+            return val2;
         }
         
 });
 });
 function add(){
-    console.log('gg');
     val1=parseFloat(val1);
     val2=parseFloat(val2);
-    plus= val1+val2;
-    console.log(plus);
-    data.innerText=plus;
-    val1=plus;
+    data.innerText=val1+val2;
+    val1='';
     val2='';
+    operator=null;
 
     }
 function sub(){
-    console.log('ss');
     val1=parseFloat(val1);
     val2=parseFloat(val2);
-    minus= val1-val2;
-    console.log(minus);
-    data.innerText=minus;
+    data.innerText=val1-val2;
     val1=data.innerText;
     val2='';
     }
        
 function product(){
-    console.log('mm');
+  
     val1=parseFloat(val1);
     val2=parseFloat(val2);
-    multiply= val1*val2;
-    console.log(multiply);
-    data.innerText=multiply;
+    data.innerText=val1*val2;
     val1=data.innerText;
     val2='';
     }
                 
 function divide(){
-    console.log('dd');
     val1=parseFloat(val1);
     val2=parseFloat(val2);
-    division= val1/val2;
-    console.log(division);
-    data.innerText=division;
+    data.innerText=val1/val2;
     val1=data.innerText;
     val2='';
     }
